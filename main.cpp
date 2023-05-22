0a3b4eec6c2f203a76868fd082f5855913d0362a
emmanuel-va1d <vald.emmanuel04@gmail.com>
2023-05-22 13:19:30 -0700

    Started working on a class to handle the cards. The class contains the values of the cards value, suit and name. In the main function, I tested assigning each card and then printing each using a quick function.

--- main.cpp
+++ main.cpp
@@ -6,12 +6,43 @@
 using namespace std;
 
 /* arrays holding card values, names and suit */
-const int CARD_VALUES[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10};
-const string CARD_NAMES[] = {"ACE", "ONE", "TWO", "THREE", "FOUR", 
+const int VALUES[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10};
+const string NAMES[] = {"ACE", "TWO", "THREE", "FOUR", 
                             "FIVE", "SIX", "SEVEN", "EIGHT", "NINE", 
                             "TEN", "KING", "QUEEN", "JACK"};
-const string CARD_SUIT[] = {"SPADES", "CLUBS", "HEARTS", "DIAMOND"};
+const string SUIT[] = {"SPADES", "CLUBS", "HEARTS", "DIAMOND"};
+
+/* class to handle cards */
+class Cards {
+    private:
+        int value;
+        string suit,
+               name;
+    public:
+        void printDeck() {
+            cout << name << endl;
+        }
+        Cards(const int& value, const string& suit, const string& name) {
+            this->value = value;
+            this->suit = suit;
+            this->name = suit + " OF " + name;
+        }
+        Cards() {
+            value = 0;
+            suit = "";
+            name = "";
+        }
+};
 
 int main() {
+    Cards card[4][13];
+
+    for (int i = 0; i < 4; i++) {
+        for (int j = 0; j < 13; j++) {
+            card[i][j] = Cards(VALUES[j], NAMES[j], SUIT[i]);
+            card[i][j].printDeck();
+        }
+    }
+
     return 0;
 }
